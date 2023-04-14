caa8898b301d3b9234f221d3d42bea150b3505d5
yanquankun <17600610907@163.com>
2023-04-14 18:56:21 +0800

    feat:commit

--- Array/entries.js
+++ Array/entries.js
@@ -3,6 +3,19 @@
  * @Desc: 方法返回一个新的数组迭代器对象，该对象包含数组中每个索引的键/值对
  * @Params: {}
  */
+
+const nodeList = function (val, next) {
+  this.val = val || undefined;
+  this.next = next || null;
+};
 Array.prototype.entries = function () {
   const arr = this;
+  let obj = {};
+  for (let index of arr) {
+    obj.next = {
+      value: [index, arr[index]],
+      done: index == arr.length,
+    };
+  }
+  return node;
 };
